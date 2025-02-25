70412418d72f6488b6a709664182aa1e8167191e
HeadlessWing <wingcue@gmail.com>
2025-02-25 13:35:28 -0800

    added player files and code to draw player in screen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 
 def main():
@@ -10,14 +12,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0 #delta time
-    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     #Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        #limits framerate to 60 fps
         dt = clock.tick(60)/1000
         
 
