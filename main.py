6711479248eb10c4fff8ca9abb95d9d2fed3f7ce
HeadlessWing <wingcue@gmail.com>
2025-02-26 12:42:56 -0800

    added forward and backward movement

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from player import *
 def main():
     pygame.init
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0 #delta time
